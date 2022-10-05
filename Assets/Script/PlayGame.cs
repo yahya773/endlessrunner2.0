65cd12725152b8083230305ee965cd138549de27
yahya773 <113103344+yahya773@users.noreply.github.com>
2022-10-05 12:15:16 +0100

    button
    added a button

---
+++ Assets/Script/PlayGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour
+{
+
+    public void play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+}
